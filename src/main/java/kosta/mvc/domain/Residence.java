e92fc319222ab926614801fe3ca59fd9f7938eb4
seryun <zzangah99@naver.com>
2022-12-08 16:56:30 +0900

    tiles 수정, 버튼 경로

--- src/main/java/kosta/mvc/domain/Residence.java
+++ src/main/java/kosta/mvc/domain/Residence.java
@@ -77,8 +77,8 @@ public class Residence {
 
 
 	
-	@OneToMany(mappedBy = "residence", cascade = CascadeType.ALL) 
-	private List<Reservation> reservationList;
+	/*@OneToMany(mappedBy = "residence", cascade = CascadeType.ALL) 
+	private List<Reservation> reservationList;*/
 	
 	@OneToMany(mappedBy = "residence", cascade = CascadeType.ALL) 
 	private List<ResidenceDetail> residenceDetailList;
